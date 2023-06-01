82bc6a224ac3568a64c0153192b58fe9437e8ec5
JJOlatunji4 <133688803+JJOlatunji4@users.noreply.github.com>
2023-06-01 09:04:33 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+let startTime;
+let endTime;
+let reactionTime;
+
+document.getElementById('game-area').addEventListener('click', function() {
+  if (startTime) {
+    endTimer();
+    showResult();
+  } else {
+    startTimer();
+  }
+});
+
+function startTimer() {
+  startTime = new Date().getTime();
+  document.getElementById('game-area').style.backgroundColor = 'red';
+}
+
+function endTimer() {
+  endTime = new Date().getTime();
+  reactionTime = (endTime - startTime) / 1000;
+  startTime = null;
+}
+
+function showResult() {
+  document.getElementById('result').innerText = 'Your reaction time: ' + reactionTime + 's';
+  document.getElementById('result').style.display = 'block';
+  document.getElementById('game-area').style.background = '#f0f0f0';
+}
